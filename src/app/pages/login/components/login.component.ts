21ecf0f87cc201a5bf04e1323abb438f4f04a8f5
skandha2103 <skandha92@gmail.com>
2022-11-14 23:28:15 +0530

    Login changes from sk

--- src/app/pages/login/components/login.component.ts
+++ src/app/pages/login/components/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { first } from 'rxjs';
 import { AuthenticationService } from 'src/app/services/auth.service';
@@ -7,59 +12,62 @@ import { AuthenticationService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   loginForm: FormGroup = new FormGroup({});
   loading = false;
   submitted = false;
   error = '';
+  abc: string = '';
 
   constructor(
-      private formBuilder: FormBuilder,
-      private route: ActivatedRoute,
-      private router: Router,
-      private authenticationService: AuthenticationService
-  ) { 
-      // redirect to home if already logged in
-    //   if (this.authenticationService.currentUserValue) { 
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    // redirect to home if already logged in
+    //   if (this.authenticationService.currentUserValue) {
     //       this.router.navigate(['/']);
     //   }
   }
 
   ngOnInit() {
-      this.loginForm = this.formBuilder.group({
-          username: ['', Validators.required],
-          password: ['', Validators.required]
-      });
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
   // convenience getter for easy access to form fields
-  get f() { return this.loginForm.controls; }
-
-  onSubmit() {
-      this.submitted = true;
-
-      // stop here if form is invalid
-      if (this.loginForm.invalid) {
-          return;
-      }
-
-      this.loading = true;
-      this.authenticationService.login(this.f.username.value,this.f.password.value)
-          .pipe(first())
-          .subscribe({
-              next: () => {
-                  // get return url from route parameters or default to '/dashboard/home'
-                  const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/dashboard/home';
-                  this.router.navigate([returnUrl]);
-              },
-              error: (error:any) => {
-                  this.error = error;
-                  this.loading = false; 
-              }
-          });
+  get f() {
+    return this.loginForm.controls;
   }
 
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.authenticationService
+      .login(this.f.username.value, this.f.password.value)
+      .pipe(first())
+      .subscribe({
+        next: () => {
+          // get return url from route parameters or default to '/dashboard/home'
+          const returnUrl =
+            this.route.snapshot.queryParams['returnUrl'] || '/dashboard/home';
+          this.router.navigate([returnUrl]);
+        },
+        error: (error: any) => {
+          this.error = error;
+          this.loading = false;
+        },
+      });
+  }
 }
